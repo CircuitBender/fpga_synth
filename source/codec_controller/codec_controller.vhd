2ba6de38091ef11d4a4790b220abea049514abe2
CircuitBender <info@marconicolasheinzen.com>
2019-03-15 09:51:28 +0100

    Add files via upload

--- source/codec_controller/codec_controller.vhd
+++ source/codec_controller/codec_controller.vhd
@@ -32,6 +32,7 @@ use work.reg_table_pkg.all; -- predifined registry with bits set for different a
 -- depending on the switch combination, different codec presets are loaded via i2c
 -- the fsm state machine controls the i2c bus and loads the preset and decides if the acknowledge bit is correctly set
 -- / if the communication was succesfull
+-- the adress_pointerer is used to adress the array which itself generates the adress for the i2c bus 
 
 
 entity codec_controller is
@@ -59,30 +60,29 @@ TYPE	codec_status	is	(idle, prepare, startWrite, waitAck);
 -- Signals
 SIGNAL currentStat		 :	codec_status; -- signal: state of fsm machine
 SIGNAL nextStat			 :	codec_status; -- signal: next state of the fsm machine
-SIGNAL count		   	 : 	unsigned(6 downto 0); -- signal: 7bit count register
-SIGNAL nextcount	 	 : 	unsigned(6 downto 0); -- signal: next 7bit count register
+SIGNAL adress_pointer		   	 : 	unsigned(6 downto 0); -- signal: 7bit adress_pointer register
+SIGNAL next_adress_pointer	 	 : 	unsigned(6 downto 0); -- signal: next 7bit adress_pointer register
 SIGNAL event_control       :	std_logic_vector(2 downto 0); -- signal: event_control register consist of the 3bit switches input
-SIGNAL next_event_control  :	std_logic_vector(2 downto 0); -- signal: next_event_control next rei^gister 3bit switches
+SIGNAL next_event_control  :	std_logic_vector(2 downto 0); -- signal: next_event_control next register 3bit switches
 SIGNAL write_data_buffer : std_logic_vector(15 downto 0); -- signal: write output buffer register
 SIGNAL next_write_data_buffer : std_logic_vector(15 downto 0); -- signal: next write output buffer register
 
 BEGIN
 
-
-	-- Process for FSM and count
-	clocked : PROCESS(clk, reset_n, nextStat)
+	-- Process for FSM and adress_pointer
+	clocked : PROCESS(ALL)
 	BEGIN
-		-- Reset for Counter, Output and FSM , override with '0'
+		-- Reset for adress_pointerer, Output and FSM , override with '0'
 		IF reset_n = '0' THEN
 			currentStat <= idle;
 			event_control <= (OTHERS => '0');
-			count <= (OTHERS => '0');
+			adress_pointer <= (OTHERS => '0');
 			write_data_buffer <= (OTHERS => '0');
 		
-		-- on rising edge states are shifted and count is increased
+		-- on rising edge states are shifted and adress_pointer is increased
 		ELSIF rising_edge(clk) THEN
 			currentStat <= nextStat;
-			count <= nextcount;
+			adress_pointer <= next_adress_pointer;
 			event_control <= next_event_control;
 			write_data_buffer <= next_write_data_buffer;
 		END IF;
@@ -95,22 +95,22 @@ BEGIN
 	BEGIN
 	-- Default Statements
 	nextStat <= currentStat;
-	nextcount <= count;
+	next_adress_pointer <= adress_pointer;
 	next_event_control <= event_control;
 	next_write_data_buffer <= write_data_buffer;
 	write_o <= '0';
 
 	CASE currentStat IS
 	WHEN idle => -- default state
-		next_event_control <= sw_sync_i;
+		next_event_control <= sw_sync_i; -- reading from the input switches bus 
 		IF initialize_i = '0' THEN -- change state to prepare if initialize_i signal is '1'
 			nextStat <= prepare;
 			CASE event_control IS -- set array entry to 9
-				WHEN "000" => nextcount <= to_unsigned(9,7);
-				WHEN "001" => nextcount <= to_unsigned(9,7);
-				WHEN "010" => nextcount <= to_unsigned(9,7);
-				WHEN "011" => nextcount <= to_unsigned(9,7);
-				WHEN "100" => nextcount <= to_unsigned(9,7);
+				WHEN "000" => next_adress_pointer <= to_unsigned(9,7);
+				WHEN "001" => next_adress_pointer <= to_unsigned(9,7);
+				WHEN "010" => next_adress_pointer <= to_unsigned(9,7);
+				WHEN "011" => next_adress_pointer <= to_unsigned(9,7);
+				WHEN "100" => next_adress_pointer <= to_unsigned(9,7);
 				WHEN OTHERS => nextStat <= idle;
 			END CASE;
 		END IF;
@@ -119,15 +119,15 @@ BEGIN
 		nextStat <= startWrite;
 		-- writing data in output register
 		CASE event_control IS	-- switch between different stored commands
-			WHEN "000" => next_write_data_buffer <= std_logic_vector(count) & C_W8731_ANALOG_BYPASS(to_integer(count));
-			WHEN "001" => next_write_data_buffer <= std_logic_vector(count) & C_W8731_ANALOG_MUTE_RIGHT(to_integer(count));
-			WHEN "010" => next_write_data_buffer <= std_logic_vector(count) & C_W8731_ANALOG_MUTE_LEFT(to_integer(count));
-			WHEN "011" => next_write_data_buffer <= std_logic_vector(count) & C_W8731_ANALOG_MUTE_BOTH(to_integer(count));
-			WHEN "100" => next_write_data_buffer <= std_logic_vector(count) & C_W8731_ADC_DAC_0DB_48K(to_integer(count));
+			WHEN "000" => next_write_data_buffer <= std_logic_vector(adress_pointer) & C_W8731_ANALOG_BYPASS(to_integer(adress_pointer));
+			WHEN "001" => next_write_data_buffer <= std_logic_vector(adress_pointer) & C_W8731_ANALOG_MUTE_RIGHT(to_integer(adress_pointer));
+			WHEN "010" => next_write_data_buffer <= std_logic_vector(adress_pointer) & C_W8731_ANALOG_MUTE_LEFT(to_integer(adress_pointer));
+			WHEN "011" => next_write_data_buffer <= std_logic_vector(adress_pointer) & C_W8731_ANALOG_MUTE_BOTH(to_integer(adress_pointer));
+			WHEN "100" => next_write_data_buffer <= std_logic_vector(adress_pointer) & C_W8731_ADC_DAC_0DB_48K(to_integer(adress_pointer));
 
 			WHEN OTHERS =>
 				nextStat <= idle;
-				nextcount <= (OTHERS => '0');
+				next_adress_pointer <= (OTHERS => '0');
 		END CASE;
 
 	-- start sending data
@@ -138,12 +138,12 @@ BEGIN
 	-- wait till data are received
 	WHEN waitAck =>
 		IF (write_done_i = '1') THEN -- sending next data package
-			IF (count = 0) THEN -- go to idle if all data is sent
+			IF (adress_pointer = 0) THEN -- go to idle if all data is sent
 				nextStat <= idle;
 				next_write_data_buffer <= (OTHERS => '0');
 			ELSE
 				nextStat <= prepare;
-				nextcount <= count -1; -- reduce count by 1
+				next_adress_pointer <= adress_pointer -1; -- reduce adress_pointer by 1
 			END IF;
 		ELSIF (ack_error_i = '1') THEN -- resending data if error is received
 			nextStat <= prepare;
