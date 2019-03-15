c94c16d454abd6270cc2c1f880521d18ce46315d
CircuitBender <info@marconicolasheinzen.com>
2019-03-15 10:21:45 +0100

    schawan

---
+++ source/infrastructure.vhd
@@ -0,0 +1,127 @@
+-------------------------------------------------------------------------------
+-- Title      : 
+-- Project    : 
+-------------------------------------------------------------------------------
+-- File       : infrastructure.vhd
+-- Author     :   <Schawan@LAPTOP-PV1H37B3>
+-- Company    : 
+-- Created    : 2019-03-13
+-- Last update: 2019-03-13
+-- Platform   : 
+-- Standard   : VHDL'08
+-------------------------------------------------------------------------------
+-- Description: 
+-------------------------------------------------------------------------------
+-- Copyright (c) 2019 
+-------------------------------------------------------------------------------
+-- Revisions  :
+-- Date        Version  Author  Description
+-- 2019-03-13  1.0      Schawan	Created
+-------------------------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+-------------------------------------------------------------------------------
+
+entity infrastructure is
+  port(
+  CLOCK_50:in std_logic;
+  KEY_0:in std_logic;
+  KEY_1: in std_logic;
+  SW:in std_logic_vector(17 downto 0);
+  GPIO_26:in std_logic;
+  clk_12m_int: out std_logic;
+  reset_n:out std_logic;
+  key_1_sync: out std_logic;
+  sw_sync: out std_logic_vector (17 downto 0);
+  gpio_26_sync: out std_logic
+    );
+
+end entity infrastructure;
+
+-------------------------------------------------------------------------------
+
+architecture str of infrastructure is
+
+  -----------------------------------------------------------------------------
+  -- Internal signal declarations
+  -----------------------------------------------------------------------------
+
+  -----------------------------------------------------------------------------
+  -- Component declarations
+  -----------------------------------------------------------------------------
+
+  component synchronize is
+    generic (
+      width : positive);
+    port (
+      signal_i : in  std_logic_vector(width-1 downto 0);
+      clk_12m  : in  std_logic;
+      signal_o : out std_logic_vector(width-1 downto 0));
+  end component synchronize;
+
+  component modulo_divider is
+    generic (
+      width : positive);
+    port (
+      clk, reset_n : in  std_logic;
+      clk_12m      : out std_logic);
+  end component modulo_divider;
+
+begin  -- architecture str
+
+  -----------------------------------------------------------------------------
+  -- Component instantiations
+  -----------------------------------------------------------------------------
+
+  -- instance "synchronize_1"
+  synchronize_1: synchronize
+    generic map (
+      width => width)
+    port map (
+      signal_i => KEY_1,
+      clk_12m  => clk_12m,
+      signal_o => key_1_sync);
+
+  -- instance "synchronize_2"
+  synchronize_2: synchronize
+    generic map (
+      width => width)
+    port map (
+      signal_i => SW,
+      clk_12m  => clk_12m,
+      signal_o => sw_sync);
+
+  -- instance "synchronize_3"
+  synchronize_3: synchronize
+    generic map (
+      width => width)
+    port map (
+      signal_i => GPIO_26i,
+      clk_12m  => clk_12m,
+      signal_o => gpio_26_sync);
+
+  -- instance "modulo_divider_1"
+  modulo_divider_1: modulo_divider
+    generic map (
+      width => width)
+    port map (
+      clk     => CLOCK_50,
+     reset_n => '1',
+      clk_12m => clk_12m);
+
+  -- instance "synchronize_4"
+  synchronize_4: synchronize
+    generic map (
+      width => width)
+    port map (
+      signal_i => KEY_0,
+      clk_12m  => clk_12m,
+      signal_o => reset_n);
+  
+
+end architecture str;
+
+-------------------------------------------------------------------------------
--- source/synthi_top.vhd
+++ source/synthi_top.vhd
@@ -6,7 +6,7 @@
 -- Author     :   <Hans-Joachim@GELKE-LENOVO>
 -- Company    : 
 -- Created    : 2018-03-08
--- Last update: 2019-03-04
+-- Last update: 2019-03-13
 -- Platform   : 
 -- Standard   : VHDL'08
 -------------------------------------------------------------------------------
@@ -25,6 +25,7 @@ use ieee.std_logic_1164.all;
 library work;
 use ieee.numeric_std.all;
 use work.reg_table_pkg.all;
+use work.tone_gen_pkg.all;
 
 -------------------------------------------------------------------------------
 
@@ -61,11 +62,94 @@ architecture struct of synthi_top is
   -----------------------------------------------------------------------------
   -- Internal signal declarations
   -----------------------------------------------------------------------------
-
+  signal reset_n: std_logic;
+  signal sw_syncro: std_loigc_vector (2 downto 0);
+  signal key_1_sync:std_logic;
+  signal write_done:std_logic_vecotr (15 downto 0);
+  signal  write_data:std_logic;
+signal    write_c:std_logic;
+signal    ack_error:std_logic;
+signal mute:std_logic;
+signal gpio_26_sync:std_logic; 
   -----------------------------------------------------------------------------
   -- Component declarations
   -----------------------------------------------------------------------------
 
+  component infrastructure is
+    port (
+      CLOCK_50     : in  std_logic;
+      KEY_0        : in  std_logic;
+      KEY_1        : in  std_logic;
+      SW           : in  std_logic_vector(17 downto 0);
+      GPIO_26      : in  std_logic;
+      clk_12m_int  : out std_logic;
+      reset_n      : out std_logic;
+      key_1_sync   : out std_logic;
+      sw_sync      : out std_logic_vector (17 downto 0);
+      gpio_26_sync : out std_logic);
+  end component infrastructure;
+
+  component codec_controller is
+    port (
+      sw_sync_i    : in  std_logic_vector(2 downto 0);
+      initialize_i : in  std_logic;
+      write_done_i : in  std_logic;
+      ack_error_i  : in  std_logic;
+      write_data_o : out std_logic_vector(15 downto 0);
+      write_o      : out std_logic;
+      mute_o       : out std_logic);
+  end component codec_controller;
+
+  component i2c_master is
+    port (
+      clk          : in    std_logic;
+      reset_n      : in    std_logic;
+      write_i      : in    std_logic;
+      write_data_i : in    std_logic_vector(15 downto 0);
+      sda_io       : inout std_logic;
+      scl_o        : out   std_logic;
+      write_done_o : out   std_logic;
+      ack_error_o  : out   std_logic);
+  end component i2c_master;
+  
+begin
+
+  -- instance "infrastructure_1"
+  infrastructure_1: infrastructure
+    port map (
+      CLOCK_50     => CLOCK_50,
+      KEY_0        => KEY_0,
+      KEY_1        => KEY_1,
+      SW           => SW,
+      GPIO_26      => GPIO_26,
+      clk_12m_int  => AUD_XCK,
+      reset_n      => reset_n,
+      key_1_sync   => key_1_sync,
+      sw_sync      => sw_syncro,
+      gpio_26_sync => gpio_26_sync);
+
+  -- instance "codec_controller_1"
+  codec_controller_1: codec_controller
+    port map (
+      sw_sync_i    => sw_syncro,
+      initialize_i => key_1_sync,
+      write_done_i => write_done,
+      ack_error_i  => ack_error,
+      write_data_o => write_data,
+      write_o      => write_c,
+		mute_o       => mute);
+
+  -- instance "i2c_master_1"
+  i2c_master_1: i2c_master
+    port map (
+      clk          => AUD_CXK,
+      reset_n      => reset_n,   
+      write_i      => write_c,
+      write_data_i => write_data,
+      sda_io       => I2C_SDAT,
+      scl_o        => I2C_SCLK,
+      write_done_o => write_done,
+      ack_error_o  => ack_error);
 
 end architecture struct;
 
