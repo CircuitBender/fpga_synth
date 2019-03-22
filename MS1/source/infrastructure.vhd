4575e232ed4c254f0323e6047441e6f562d14c43
Cloede <48350367+Cloede@users.noreply.github.com>
2019-03-22 10:13:48 +0100

    Add files via upload
    Testbench updated

--- MS1/source/infrastructure.vhd
+++ MS1/source/infrastructure.vhd
@@ -1,12 +1,12 @@
 -------------------------------------------------------------------------------
--- Title      : 
--- Project    : 
+-- Title      : infrastructure
+-- Project    : synthi_project
 -------------------------------------------------------------------------------
 -- File       : infrastructure.vhd
--- Author     :   <Schawan@LAPTOP-PV1H37B3>
+-- Author     :   <Nicola@NICOLA>
 -- Company    : 
--- Created    : 2019-03-13
--- Last update: 2019-03-13
+-- Created    : 2019-03-08
+-- Last update: 2019-03-15
 -- Platform   : 
 -- Standard   : VHDL'08
 -------------------------------------------------------------------------------
@@ -16,27 +16,26 @@
 -------------------------------------------------------------------------------
 -- Revisions  :
 -- Date        Version  Author  Description
--- 2019-03-13  1.0      Schawan	Created
+-- 2019-03-08  1.0      Nicola	Created
 -------------------------------------------------------------------------------
 
 library ieee;
 use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
 
 -------------------------------------------------------------------------------
 
 entity infrastructure is
-  port(
-  CLOCK_50:in std_logic;
-  KEY_0:in std_logic;
-  KEY_1: in std_logic;
-  SW:in std_logic_vector(17 downto 0);
-  GPIO_26:in std_logic;
-  clk_12m: out std_logic;
-  reset_n:out std_logic;
-  key_1_sync: out std_logic;
-  sw_sync: out std_logic_vector (17 downto 0);
-  gpio_26_sync: out std_logic
+
+  port (
+    clock_50 		: in std_logic;
+    key      		: in std_logic_vector(1 downto 0);
+    sw       		: in std_logic_vector(17 downto 0);
+    gpio_26  		: in std_logic;
+    clk_12m  		: out std_logic;
+    key_1_sync 	: out std_logic;
+    reset_n  		: out std_logic;
+    sw_sync  		: out std_logic_vector(17 downto 0);
+    gpio_26_sync 	: out std_logic
     );
 
 end entity infrastructure;
@@ -48,12 +47,11 @@ architecture str of infrastructure is
   -----------------------------------------------------------------------------
   -- Internal signal declarations
   -----------------------------------------------------------------------------
-signal clk_12m_int:std_logic;
-
+  signal clk_12m_int : std_logic;   -- internal 12.5MHz clock-signal
+  signal key_sync_int : std_logic_vector(1 downto 0);  --synchronized key-signals
   -----------------------------------------------------------------------------
   -- Component declarations
   -----------------------------------------------------------------------------
-
   component synchronize is
     generic (
       width : positive);
@@ -62,19 +60,13 @@ signal clk_12m_int:std_logic;
       clk_12m  : in  std_logic;
       signal_o : out std_logic_vector(width-1 downto 0));
   end component synchronize;
-  
-    component synchronize_single is
-    port (
-      signal_i : in  std_logic;
-      clk_12m  : in  std_logic;
-      signal_o : out std_logic);
-  end component synchronize_single;
 
   component modulo_divider is
     generic (
       width : positive);
     port (
-      clk, reset_n : in  std_logic;
+      clk          : in  std_logic;
+      reset_n      : out std_logic;
       clk_12m      : out std_logic);
   end component modulo_divider;
 
@@ -83,48 +75,42 @@ begin  -- architecture str
   -----------------------------------------------------------------------------
   -- Component instantiations
   -----------------------------------------------------------------------------
-
-  -- instance "synchronize_1"
-  synchronize_1: synchronize_single
-
-    port map (
-      signal_i => KEY_1,
-      clk_12m  => clk_12m_int,
-      signal_o => key_1_sync);
-
-  -- instance "synchronize_2"
-  synchronize_2: synchronize
-    generic map (
-      width => 18)
-    port map (
-      signal_i => SW,
-      clk_12m  => clk_12m_int,
-      signal_o => sw_sync);
-
-  -- instance "synchronize_3"
-  synchronize_3: synchronize_single
-    port map (
-      signal_i => GPIO_26,
-      clk_12m  => clk_12m_int,
-      signal_o => gpio_26_sync);
-
-  -- instance "modulo_divider_1"
-  modulo_divider_1: modulo_divider
+  sync_inst_1: synchronize
     generic map (
       width => 2)
     port map (
-      clk     => CLOCK_50,
-     reset_n => '1',
+      signal_i => key,
+      clk_12m  => clk_12m_int,
+      signal_o => key_sync_int);
+  
+  sync_inst_2: synchronize
+    generic map (
+      width => 18)
+    port map (
+      signal_i => sw,
+      clk_12m  => clk_12m_int,
+      signal_o => sw_sync);
+  
+  sync_inst_3: synchronize
+    generic map (
+      width => 1)
+    port map (
+      signal_i(0) => gpio_26,
+      clk_12m  => clk_12m_int,
+      signal_o(0) => gpio_26_sync);
+
+    takt_inst: modulo_divider
+    generic map (
+      width => 2)
+    port map (
+      clk     => clock_50,
       clk_12m => clk_12m_int);
 
-  -- instance "synchronize_4"
-  synchronize_4: synchronize_single
-    port map (
-      signal_i => KEY_0,
-      clk_12m  => clk_12m_int,
-      signal_o => reset_n);
   
-
+  clk_12m <= clk_12m_int;               --12.5MHz clock output
+  key_1_sync <= key_sync_int(1);        --synchronized keys get split in 2 outputs
+  reset_n <= key_sync_int(0);
+  
 end architecture str;
 
 -------------------------------------------------------------------------------
